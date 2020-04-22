b417af913a7a4852b14286f31127034d8922732d
barry <410547@gm.tnfsh.tn.edu.tw>
2020-04-22 16:59:00 +0800

    115

--- src/model_deploy/main.cpp
+++ src/model_deploy/main.cpp
@@ -158,7 +158,7 @@ int main(int argc, char* argv[]) {
 
   // needed by this graph.
 
-  static tflite::MicroOpResolver<5> micro_op_resolver;
+  static tflite::MicroOpResolver<6> micro_op_resolver;
 
   micro_op_resolver.AddBuiltin(
 
@@ -182,7 +182,8 @@ int main(int argc, char* argv[]) {
 
                                tflite::ops::micro::Register_SOFTMAX());
 
-
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
+                             tflite::ops::micro::Register_RESHAPE(), 1);
   // Build an interpreter to run the model with
 
   static tflite::MicroInterpreter static_interpreter(
